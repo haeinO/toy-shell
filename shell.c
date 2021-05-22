476db8a5fad98d0e50766fbf523bdf4a0a4bd5d5
haeinO <84624932+haeinO@users.noreply.github.com>
2021-05-23 02:53:54 +0900

    Update shell.c

--- shell.c
+++ shell.c
@@ -5,8 +5,34 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <pwd.h>
 
 #define MAX_LEN_LINE    100
+#define LEN_HOSTNAME    30
+
+int hostname(char *args[])
+{   
+    if (strcmp(args[0], "HOSTNAME") == 0)
+    {
+        char hostname[LEN_HOSTNAME + 1];
+        memset(hostname, 0x00, sizeof(hostname));
+        printf("username: %s\n", getpwuid(getuid())->pw_name);
+        gethostname(hostname, LEN_HOSTNAME);
+        printf("hostname: %s\n", hostname);
+        exit(1);
+    }
+    return 0;
+}
+
+int exit_program(char *args[])
+{
+    if (strcmp(args[0], "exit") == 0)
+    {
+        printf("EXIT\n");
+        exit(2);
+    }
+    return 0;
+}
 
 int main(void)
 {
@@ -19,6 +45,9 @@ int main(void)
         char *s;
         int len;
         
+        gethostname(hostname, LEN_HOSTNAME);
+        printf("hostname: %s/n", hostname);
+    
         printf("MyShell $ ");
         s = fgets(command, MAX_LEN_LINE, stdin);
         if (s == NULL) {
@@ -32,6 +61,8 @@ int main(void)
             command[len - 1] = '\0'; 
         }
         
+        hostname(args);
+        exit_program(args);
         printf("[%s]\n", command);
       
         pid = fork();
